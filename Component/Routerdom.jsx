096057cd53d3cd9f688e8b40508f63cb9f0228cf
Ankita-patel2702 <ankitapatel2772@gmail.com>
2025-02-19 19:09:53 +0530

    Add files via upload

---
+++ Component/Routerdom.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+//  Task 1: Set up a basic React Router with two routes: one for a Home page and one for an
+// About page. Display the appropriate content based on the URL.
+
+//  Task 2: Create a navigation bar using React Router’s Link component that allows users to
+// switch between the Home, About, and Contact pages.
+
+
+    const Home = () => <h2>Home Page</h2>;
+    const About = () => <h2>About Page</h2>;
+    const Contact = () => <h2>Contact Page</h2>;
+
+
+    const Routerdom = () => {
+      return (
+        <Router>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+      );
+    }
+
+export default Routerdom;
